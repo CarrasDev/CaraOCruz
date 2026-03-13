0ddbd5ddbcdb5d7e8eaf2d8e2db083b437f37422
CarrasDev <carrascoluque@gmail.com>
2026-03-13 16:37:12 +0100

    Limpiar imports innecesarios

--- app/src/main/java/com/example/caraocruz/PresentationActivity.kt
+++ app/src/main/java/com/example/caraocruz/PresentationActivity.kt
@@ -1,28 +1,16 @@
 package com.example.caraocruz
 
 import androidx.appcompat.app.AppCompatActivity
-import android.annotation.SuppressLint
 import android.content.Intent
-import android.os.Build
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.view.MotionEvent
-import android.view.View
-import android.view.WindowInsets
-import android.widget.LinearLayout
-import android.widget.TextView
 import com.example.caraocruz.databinding.ActivityPresentationBinding
 
-/**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
- */
 class PresentationActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPresentationBinding
 
-    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 

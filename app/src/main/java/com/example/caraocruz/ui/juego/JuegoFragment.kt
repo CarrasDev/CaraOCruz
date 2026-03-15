5358ff80dc389861474c22afc50426e51a4b0b70
CarrasDev <carrascoluque@gmail.com>
2026-03-15 21:38:00 +0100

    Crear JuegoFragment.kt

---
+++ app/src/main/java/com/example/caraocruz/ui/juego/JuegoFragment.kt
@@ -0,0 +1,53 @@
+package com.example.caraocruz.ui.juego
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import com.example.caraocruz.R
+import kotlinx.coroutines.launch
+
+class JuegoFragment : Fragment(R.layout.fragment_juego) {
+
+    private var _binding: FragmentJuegoBinding? = null
+    private val binding get() = _binding!!
+
+    private val viewModel: JuegoViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentJuegoBinding.bind(view)
+
+        // Suscripción a los cambios en monedas:
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.monedas.collect { saldo ->
+                binding.tvSaldo.txt = "Saldo: $saldo monedas"
+            }
+        }
+
+        // Suscripción a los cambios en resultadoMensaje:
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.resultadoMensaje.collect { mensaje ->
+                binding.tvMensaje.txt = mensaje
+            }
+
+        }
+
+        binding.btnCara.setOnClickListener { procesarJugada(true) }
+        binding.btnCruz.setOnClickListener { procesarJugada(false) }
+
+
+    }
+
+    private fun procesarJugada(esCara: Boolean) {
+        val apuestaText = binding.etApuesta.text.toString()
+        val apuesta = apuestaText.toIntOrNull() ?: 0
+        viewModel.jugar(apuesta, esCara)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}

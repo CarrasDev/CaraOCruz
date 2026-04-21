0bdceec92bada002dbd71f416ac767eacadef9a2
CarrasDev <carrascoluque@gmail.com>
2026-04-21 23:33:58 +0200

    Crear HelpFraqgment.kt

---
+++ app/src/main/java/com/example/caraocruz/ui/menu/HelpFragment.kt
@@ -0,0 +1,30 @@
+package com.example.caraocruz.ui.menu
+
+import android.os.Bundle
+import android.view.View
+import android.webkit.WebViewClient
+import androidx.fragment.app.Fragment
+import com.example.caraocruz.databinding.FragmentHelpBinding
+
+class HelpFragment : Fragment() {
+
+    private var _binding: FragmentHelpBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding = FragmentHelpBinding.bind(view)
+
+        // Configuración
+        binding.webViewHelp.webViewClient = WebViewClient()
+        binding.webViewHelp.settings.javaScriptEnabled = true
+
+        // TODO: Cargo www.google.com para prueba, substituir por un archivo local
+        binding.webViewHelp.loadUrl("http://www.google.com")
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
